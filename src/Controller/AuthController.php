4a4117d1f2aa0915274021b3fae8c21f2ad4f0d9
Ludo <labaude.ludo@gmail.com>
2024-07-09 11:15:08 +0200

    probleme pour la partie login invalid credentials

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -5,35 +5,32 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 
 class AuthController extends AbstractController
 {
 
-      #[Route("/api/login", name:"login", methods:["GET", "POST"])]  
-      public function login(Request $request, UserRepository $repository): Response
-      {
-          $data = json_decode($request->getContent(), true);
-          $user = $this->$repository->findOneBy(['email' => $data['email']]);
-  
-          if(!$user){
-                return new Response('user non reconnu ou identifiants incorrects');
-          }
-  
-          return new Response('User logged in successfully', 200);
+        #[Route('/api/login', name: 'api_login', methods: ['POST'])]
+             public function login(?User $user): Response
+              {
+                 if (null === $user) {
+                     return $this->json([
+                         'message' => 'missing credentials',
+                     ], Response::HTTP_UNAUTHORIZED);
+                 }
+        
+                 $token = $user->getPassword(); // somehow create an API token for $user
+        
+                  return $this->json([
+                     'message' => 'Welcome to your new controller!',
+                     'path' => 'src/Controller/ApiLoginController.php',
+                     'user'  => $user->getUserIdentifier(),
+                     'token' => $token,
+                  ]);
+              }
 
       }
-
-
-    /**
-     * @Route("/logout", name="logout", methods={"POST"})
-     */
-    public function logout(): void
-    {
-        // controller can be blank: it will never be executed!
-        throw new \Exception('Don\'t forget to activate logout in security.yaml');
-    }
-}
+     
